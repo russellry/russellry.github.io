030fe68f731888e902379e3ebf0b1bb346ec59fb
russell <digivicer@hotmail.com>
2019-10-07 12:03:57 +0800

    testing

---
+++ precache-manifest.00e70484bfbe3c5240e403d8ed97bc46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1374613e41885edcb8fe547d6b94ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d31430d97c40adb5fdb7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1145102afe459efc5fb7",
+    "url": "/static/js/2.15cbb02b.chunk.js"
+  },
+  {
+    "revision": "d31430d97c40adb5fdb7",
+    "url": "/static/js/main.91187f24.chunk.js"
+  },
+  {
+    "revision": "7c92e6c03e3c292854b9",
+    "url": "/static/js/runtime-main.48354b3d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
